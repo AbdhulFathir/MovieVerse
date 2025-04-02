6983c33995b84660e36954e919c597f0ff8096bf
AbdhulFathir <abdhulfathirworks@gmail.com>
2025-04-03 00:43:11 +0530

    implement main view

--- app/src/main/java/com/example/movieverse/MainActivity.kt
+++ app/src/main/java/com/example/movieverse/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.movieverse.ui.theme.MovieVerseTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,64 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieVerseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MainScreen()
                 }
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomButton(text = "Add Movies to DB") {
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        CustomButton(text = "Search for Movies") {
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        CustomButton(text = "Search for Actors") {
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        CustomButton(text = "Search by Title (Extra)") {
+
+        }
+
+
+    }
 }
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun GreetingPreview() {
-    MovieVerseTheme {
-        Greeting("Android")
+fun CustomButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(300.dp)
+            .height(50.dp),
+        shape = RoundedCornerShape(20.dp)
+    ) {
+        Text(text, fontSize = 18.sp)
     }
 }
